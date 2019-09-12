183bcd45b15a1081d07e208906fb7a446d010bf8
Ruth Poliakon <r.poliakon@gmail.com>
2019-09-12 17:49:17 -0600

    form validation added

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -2,28 +2,56 @@ import React from "react";
 import { withFormik, Form, Field } from "formik";
 import * as Yup from "yup";
 
-const OnboardingForm = props => {
+const OnboardingForm = ({ values, errors, touched }) => {
   return (
     <Form>
-      <Field type="text" name="name" placeholder="Name" />
-      <Field type="email" name="email" placeholder="Email" />
-      <Field type="password" name="password" placeholder="Password" />
-      <label>
-        <Field type="checkbox" name="tos" />
-        Accept TOS
-      </label>
+      <div>
+        {touched.name && errors.name && <p>{errors.name}</p>}
+        <Field type="text" name="name" placeholder="Name" />
+      </div>
+
+      <div>
+        {touched.email && errors.email && <p>{errors.email}</p>}
+        <Field type="email" name="email" placeholder="Email" />
+      </div>
+
+      <div>
+        {touched.password && errors.password && <p>{errors.password}</p>}
+        <Field type="password" name="password" placeholder="Password" />
+      </div>
+
+      <div>
+        {touched.tos && errors.tos && <p>{errors.tos}</p>}
+        <label>
+          <Field type="checkbox" name="tos" />
+          Accept Terms of Service
+        </label>
+      </div>
+
       <button>Submit</button>
     </Form>
   );
 };
 
 export default withFormik({
-  mapPropsToValues: values => {
+  mapPropsToValues: ({ name, email, password, tos }) => {
     return {
-      name: values.name || "",
-      email: values.email || "",
-      password: values.password || "",
-      tos: values.tos || false
+      name: name || "",
+      email: email || "",
+      password: password || "",
+      tos: tos || false
     };
-  }
+  },
+
+  validationSchema: Yup.object().shape({
+    name: Yup.string().required("Please enter your name"),
+    email: Yup.string()
+      .email("Please enter a valid email")
+      .required("Please enter a valid email"),
+    password: Yup.string()
+      .required("Please enter a password")
+      .min(8, "Password must be between 8 and 20 characters")
+      .max(20, "Password must be between 8 and 20 characters"),
+    tos: Yup.boolean().oneOf([true], "Please accept the Terms of Service")
+  })
 })(OnboardingForm);
